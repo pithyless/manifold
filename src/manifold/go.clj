1a3ef7dc09c0d40d24b3b79281e6a63b10aab888
Ryan Smith <rnsmith2@gmail.com>
2020-05-29 14:32:02 -0700

    Add core.async/go variant that works with defferreds
    `let-flow` explicitly expects that everything inside of it should be resolved before
    passed to other functions. Instead, let's introduce a new macro that still allows for
    normal looking clojure code, but adds ability to park when specified. Instead of writing
    our own code for this, let's take advantage of the fact that core.async's state-machine code
    is quite reusable.

---
+++ src/manifold/go.clj
@@ -0,0 +1,76 @@
+(ns ^{:author "Ryan Smith"
+      :doc    "Provide an variant of `core.async/go` that works with manifold's deferreds and executors. Utilizes core.async's state-machine generator, so core.async must be available as a dependency."}
+  manifold.go
+  (:require [manifold
+             [executor :as ex]
+             [deferred :as d]]
+            [clojure.core.async.impl
+             [ioc-macros :as ioc]]))
+
+(defn return-chan [state value]
+  (let [d (ioc/aget-object state ioc/USER-START-IDX)]
+    (d/success! d value)
+    d))
+
+(defn <!-no-throw
+  "takes value from deferred. Must be called inside a (go ...) block. Will
+  return nil if closed. Will park if nothing is available. If an error
+  is thrown inside the body, that error will be placed as the return value.
+
+  N.B. To make `go` usage idiomatic with the rest of manifold, use `<!?`
+  instead of this directly."
+  [port]
+  (assert nil "<! used not in (go ...) block"))
+
+(defmacro <!?
+  "takes a val from port. Must be called inside a (go ...) block.
+  Will park if nothing is available. If value that is returned is
+  a Throwable, will re-throw."
+  [port]
+  `(let [r# (<!-no-throw ~port)]
+     (if (instance? Throwable r#)
+       (throw r#)
+       r#)))
+
+(defn run-state-machine-wrapped [state]
+  (try (ioc/run-state-machine state)
+       (catch Throwable ex
+         (d/error! (ioc/aget-object state ioc/USER-START-IDX) ex)
+         (throw ex))))
+
+(defn take! [state blk c]
+  (let [handler (fn [x]
+                  (ioc/aset-all! state ioc/VALUE-IDX x ioc/STATE-IDX blk)
+                  (run-state-machine-wrapped state))]
+    (-> c
+        (d/chain handler)
+        (d/catch handler)))
+  (when (d/realized? c)
+    :recur))
+
+(def async-custom-terminators
+  {'manifold.go/<!-no-throw `manifold.go/take!
+   :Return                  `return-chan})
+
+(defmacro go
+  "Asynchronously executes the body, returning immediately to the
+  calling thread. Additionally, any visible calls to <!? and <!-no-throw
+  deferred operations within the body will block (if necessary) by
+  'parking' the calling thread rather than tying up an OS thread.
+  Upon completion of the operation, the body will be resumed.
+
+  Returns a deferred which will receive the result of the body when
+  completed"
+  [& body]
+  (let [crossing-env (zipmap (keys &env) (repeatedly gensym))]
+    `(let [d#                 (d/deferred)
+           captured-bindings# (clojure.lang.Var/getThreadBindingFrame)]
+       (.execute (ex/execute-pool) ^Runnable
+                 (^:once fn* []
+                   (let [~@(mapcat (fn [[l sym]] [sym `(^:once fn* [] ~(vary-meta l dissoc :tag))]) crossing-env)
+                         f# ~(ioc/state-machine `(do ~@body) 1 [crossing-env &env] async-custom-terminators)
+                         state# (-> (f#)
+                                    (ioc/aset-all! ioc/USER-START-IDX d#
+                                                   ioc/BINDINGS-IDX captured-bindings#))]
+                     (run-state-machine-wrapped state#))))
+       d#)))
